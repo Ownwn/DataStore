e7635544af6afc04f0c4d155c3597c3ef85d9fd6
Owen Suter <owen@ownwn.com>
2025-09-29 22:49:51 +1300

    file persistency

--- src/main/kotlin/com/ownwn/datastore/Controller.kt
+++ src/main/kotlin/com/ownwn/datastore/Controller.kt
@@ -6,25 +6,15 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @CrossOrigin
 class Controller {
-    private val entries: MutableList<Entry> = mutableListOf(Entry("first entry", System.currentTimeMillis()), Entry("second one here", System.currentTimeMillis()))
-
-    @GetMapping("/test")
-    fun ok(): String {
-        return "asdf ok"
-    }
-
     @GetMapping("/entries")
     fun entries(): ResponseEntity<List<Entry>> {
-        return ResponseEntity.ok(entries)
+        return ResponseEntity.ok(Database.getEntries())
     }
 
     @PostMapping("/submit")
     fun submit(@RequestBody content: String?): ResponseEntity<String> {
         return content
-            ?.takeIf { entries.add(Entry(it, System.currentTimeMillis())) }
-            ?.let {ResponseEntity.ok("ok!")}
+            ?.let { Database.addEntry(content); ResponseEntity.ok("ok!")}
             ?: ResponseEntity.badRequest().body("Bad content!")
     }
 }
-
-data class Entry(val content: String, val created_at: Long)
--- src/main/kotlin/com/ownwn/datastore/Database.kt
+++ src/main/kotlin/com/ownwn/datastore/Database.kt
@@ -1,8 +1,60 @@
 package com.ownwn.datastore
 
-class Database {
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.File
+import java.nio.file.Files
 
-    fun aa() {
+object Database {
+    val mapper = jacksonObjectMapper()
+    val dataRoot = File("data/")
+    val dbLocation = dataRoot.resolve("data.json")
+
+    init {
+        if (!dataRoot.exists()) dataRoot.mkdir()
+        dbLocation.createNewFile()
+
+        val entry = Entry("first entry", System.currentTimeMillis()-1L)
+        saveEntry(entry)
+
+        val data = Data(mutableListOf(entry))
+        saveData(data)
+
+    }
+
+    private fun getLogFile(): File {
+        val file = dataRoot.resolve("logs/").resolve(System.currentTimeMillis().toString() + ".json")
+        file.parentFile.mkdirs()
+        file.createNewFile()
+        return file
+    }
+
+    private fun saveData(data: Data) {
+        mapper.writeValue(dbLocation, data)
+    }
+
+    private fun saveEntry(entry: Entry) {
+        mapper.writeValue(getLogFile(), entry)
+    }
+
+    fun addEntry(content: String) {
+        val entry = Entry(content, System.currentTimeMillis())
+        val data = getData()
+        data.list.add(entry);
+        saveEntry(entry)
+        saveData(data)
+    }
+
+    fun getEntries(): List<Entry> {
+        return getData().list
+
+    }
+
+    private fun getData(): Data {
+        return mapper.readValue<Data>(Files.readString(dbLocation.toPath()))
 
     }
 }
+
+data class Data(val list: MutableList<Entry>)
+data class Entry(val content: String, val created_at: Long)
