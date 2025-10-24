609511c3272bb86a82188cf11f43e68c53e36ed3
Owen Suter <owen@ownwn.com>
2025-10-24 22:02:27 +1300

    add auth

---
+++ src/main/kotlin/com/ownwn/datastore/Auth.kt
@@ -0,0 +1,56 @@
+package com.ownwn.datastore
+
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import java.util.*
+
+const val loginPath: String = "/login"
+const val cookieName: String = "COOKIE_VALUE"
+
+@Component
+class Auth : Filter {
+    override fun doFilter(req: ServletRequest?,res: ServletResponse?,chain: FilterChain) {
+        if (req !is HttpServletRequest || res !is HttpServletResponse) {
+            System.err.println("Bad req")
+            return
+        }
+
+        if (loginPath == req.requestURI) {
+            chain.doFilter(req, res)
+            return
+        }
+
+        val cookies = req.cookies
+        if (cookies == null || cookies.size == 0) {
+            res.sendRedirect(loginPath)
+            return
+        }
+
+        val cookieValue = DataStoreApplication.getEnv(cookieName) ?: return res.sendError(500)
+
+        val authenticated = Arrays.stream(cookies).anyMatch { c: Cookie? -> cookieName == c?.name && cookieValue == c.value }
+        if (!authenticated) {
+            res.sendRedirect(loginPath)
+            return
+        }
+
+        chain.doFilter(req, res)
+    }
+}
+
+@Controller
+class LoginPage {
+
+    @RequestMapping(loginPath)
+    fun welcome(): String {
+        return "login"
+    }
+}
--- src/main/kotlin/com/ownwn/datastore/DataStoreApplication.kt
+++ src/main/kotlin/com/ownwn/datastore/DataStoreApplication.kt
@@ -1,11 +1,45 @@
 package com.ownwn.datastore
 
+import com.ownwn.datastore.Env2.loadEnv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.nio.file.Files
+import java.nio.file.Path
 
 @SpringBootApplication
-class DataStoreApplication
+class DataStoreApplication {
+    companion object {
+        fun getEnv(key: String): String? {
+            return Env2.envs[key]
+        }
+    }
+}
 
 fun main(args: Array<String>) {
+    loadEnv()
     runApplication<DataStoreApplication>(*args)
+}
+
+private object Env2 {
+    private val envPath = Path.of(".env")
+    val envs = mutableMapOf<String, String>()
+
+    fun loadEnv() {
+        if (!Files.exists(envPath)) {
+            throw RuntimeException("Missing .env at ${envPath.toAbsolutePath()}")
+        }
+
+        val lines = Files.readAllLines(envPath)
+        for (line in lines) {
+            if (!line.matches(Regex("^[^=]+=[^=]+$"))) { // foo=bar
+                throw RuntimeException("Bad env line $line")
+            }
+
+            val pair = line.split("=")
+            if (envs.contains(pair[0])) {
+                throw RuntimeException("Duplicate key ${pair[0]}")
+            }
+            envs[pair[0]] = pair[1]
+        }
+    }
 }
