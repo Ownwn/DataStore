6bd97445738719103351e95689c9db2e01dadb20
Owen Suter <owen@ownwn.com>
2026-07-28 15:45:38 +1200

    function lookup cache

--- src/main/kotlin/com/ownwn/server/sockets/FFIHelper.java
+++ src/main/kotlin/com/ownwn/server/sockets/FFIHelper.java
@@ -1,26 +1,46 @@
 package com.ownwn.server.sockets;
 
 import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
 
 import com.ownwn.server.java.lang.replacement.ArrayList;
+import com.ownwn.server.java.lang.replacement.HashMap;
 import com.ownwn.server.java.lang.replacement.List;
+import com.ownwn.server.java.lang.replacement.Map;
 import com.ownwn.server.java.lang.replacement.stream.IntStream;
 
 import static java.lang.foreign.ValueLayout.JAVA_INT;
 import static java.lang.foreign.ValueLayout.JAVA_SHORT;
 
 public class FFIHelper {
-    private Linker linker;
-    private SymbolLookup stdLib;
+    private static Linker linker;
+    private static SymbolLookup stdLib;
+    private static MethodHandle sendFunctionHandle;
+    private static final Map<String, MethodHandle> methodCache = new HashMap<>();
 
-    /** You must close the arena yourself! */
-    public FFIHelper() {
+    static {
         linker = Linker.nativeLinker();
         stdLib = linker.defaultLookup();
     }
 
+    /** You must close the arena yourself! */
+    public FFIHelper() {
+    }
+
     public static FFIHelper of() {
         return new FFIHelper();
+    }
+
+//    public long sendNative(int fd, MemorySegment buf, int length) {
+//
+//    }
+
+    private <T extends MemoryLayout> MethodHandle loadMethodHandle(String name, T returnType, List<T> types) {
+        return methodCache.computeIfAbsent(name, _ -> {
+            MemorySegment function_addr = stdLib.find(name).orElseThrow(() -> new RuntimeException("Can't find function of name " + name));
+            FunctionDescriptor fd = FunctionDescriptor.of(returnType, types.toArray(new MemoryLayout[types.size()]));
+            return linker.downcallHandle(function_addr, fd);
+        });
     }
 
     /** adapted from https://dev.java/learn/ffm/native/ */
@@ -29,12 +49,8 @@ public class FFIHelper {
             throw new IllegalArgumentException("Mismatch of number of args and types!");
         }
 
-        MemorySegment function_addr = stdLib.find(name).orElseThrow(() -> new RuntimeException("Can't find function of name " + name));
-
-        FunctionDescriptor fd = FunctionDescriptor.of(returnType, types.toArray(new MemoryLayout[types.size()]));
-
-        var mh = linker.downcallHandle(function_addr, fd);
-        return mh.invokeWithArguments(args.toArray(new Object[0]));
+        MethodHandle methodHandle = loadMethodHandle(name, returnType, types);
+        return methodHandle.invokeWithArguments(args.toArray(new Object[0]));
     }
 
     public <T extends MemoryLayout> Object callIntFunction(String name, T returnType, List<Integer> args) throws Throwable { // todo unnecessary copy constructor
